f998b958be066ec6167ce977bf8017828b0fe72d
Stephen C. Gilardi <scgilardi@gmail.com>
2011-05-16 05:58:56 +0000

    reformat copyright notices

--- src/artem/miglayout.clj
+++ src/artem/miglayout.clj
@@ -1,12 +1,10 @@
-;; Copyright © Stephen C. Gilardi. All rights reserved. The use and
-;; distribution terms for this software are covered by the Eclipse
-;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
-;; which can be found in the file epl-v10.html at the root of this
-;; distribution. By using this software in any fashion, you are
-;; agreeing to be bound by the terms of this license. You must not
-;; remove this notice, or any other, from this software.
-;;
-;;  artem.miglayout
+;;  Copyright © Stephen C. Gilardi. All rights reserved. The use and
+;;  distribution terms for this software are covered by the Eclipse Public
+;;  License 1.0 (http://opensource.org/licenses/eclipse-1.0.php) which can be
+;;  found in the file epl-v10.html at the root of this distribution. By using
+;;  this software in any fashion, you are agreeing to be bound by the terms of
+;;  this license. You must not remove this notice, or any other, from this
+;;  software.
 ;;
 ;;  Clojure support for the MiGLayout layout manager
 ;;  http://www.miglayout.com/
