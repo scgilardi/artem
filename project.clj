41133f860c84161e2fcd1a0d44cabcfe02a2fda7
Stephen C. Gilardi <scgilardi@gmail.com>
2011-11-09 17:58:16 -0500

    version 0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject artem "0.5-SNAPSHOT"
+(defproject artem "0.5"
   :description "Clojure support for the MiGLayout layout manager"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.miglayout/miglayout "3.7.4"]])
